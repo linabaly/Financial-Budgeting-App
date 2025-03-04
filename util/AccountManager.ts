047da64699da8203ca9913247f4a4ac975489607
Matthew <matthew@staff.libraryofcode.org>
2025-03-04 16:41:06 -0500

    fixes and add createAccount method in AccountManager.ts

--- util/AccountManager.ts
+++ util/AccountManager.ts
@@ -1,45 +1,42 @@
-import { PrismaClient } from '@prisma/client';
-import argon2 from 'argon2';
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+import { PrismaDBClient as prisma } from "../index";
+import SecurityManager from "./SecurityManager";
 
-prisma = new PrismaClient();
-const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
+const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key";
 
+export interface AccountDetails {
+  id?: string;
+  username: string;
+  password: string;
+  email: string;
+  name: string;
+  createdAt?: Date;
+  budgets?: [];
+  goals?: [];
+  recurringTransactions?: [];
+  transactions?: [];
+}
 
 export default class AccountManager {
-
-  // Create Account
-  public static async createAccount(req: any, res: any) {
-    try {
-      const { email, password, name } = req.body;
-
-      // Check if email already exists
-      const existingAccount = await prisma.account.findUnique({ where: { email } }); 
-      if (existingAccount) {
-        return res.status(400).json({ message: 'User already exists' });
-      }
+  /**
+   * This method creates a new account, provides password hashing internally, and stores the information in the database provided by the Prisma ORM.
+   * @param account An object representing the "AccountDetails" interface.
+   */
+  public static async createAccount(account: AccountDetails) {
+    // Check if email already exists
+    const existingAccount = await prisma.account.findUnique({ where: { id: account.id } });
+    if (existingAccount) {
+      throw new Error(
+        `Account with ID ${account.id} already exists, cannot create a new account for this user.`
+      );
+    }
 
     // Hash password
-    const hashedPassword = await argon2.hash(password);
+    account.password = await SecurityManager.hashPassword(account.password);
 
     // Create account
-    const account = await prisma.account.create({
-      data: {
-        email,
-        password: hashedPassword, 
-        name, 
-      },
+    return prisma.account.create({
+      data: account,
     });
-
-    res.status(201).json({ message: 'User created successfully', account});
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
-    }
   }
 
   // Get Account
@@ -48,11 +45,11 @@ export default class AccountManager {
       const { id } = req.body;
       const account = await prisma.account.findUnique({ where: { id } });
       if (!account) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({ message: "User not found" });
       }
       res.status(200).json({ account });
     } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ error: "Internal server error" });
     }
   }
 
