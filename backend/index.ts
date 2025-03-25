313ac84e7706edfb9f21ed13ef9d8f28c9da7e3b
Matthew <matthew@staff.libraryofcode.org>
2025-03-25 14:40:35 -0400

    in-line comments/documentation for index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,30 +1,22 @@
+import path from "node:path";
 import { PrismaClient } from "@prisma/client";
 import { Server } from "./util";
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import * as path from "node:path";
 
-// export let ExpressApplication: express.Application;
 export let MainServer: Server;
 export let PrismaDBClient: PrismaClient;
 
-function setupExpressApplication(): express.Application {
-  const expressApplication = express();
-  expressApplication.use(cors()); // enables CORS
-  expressApplication.use(bodyParser.json()); // parses JSON request bodies
-  expressApplication.use(bodyParser.urlencoded({ extended: true })); // parses url-encoded request bodies
-  expressApplication.use(cookieParser()); // parses cookies
-  return expressApplication;
-}
-
+/**
+ * This function returns a new Prisma DB ORM client
+ * @returns PrismaClient
+ */
 function setupPrismaClient() {
   return new PrismaClient();
 }
 
+/**
+ * This function is the entry point for the application.
+ */
 async function main() {
-  // ExpressApplication = setupExpressApplication();
   PrismaDBClient = setupPrismaClient();
   MainServer = new Server(
     Number(process.env.WEB_SERVER_PORT) || 3000,
@@ -32,12 +24,9 @@ async function main() {
   );
   MainServer.listen(Number(process.env.WEB_SERVER_PORT) || 3000);
   console.info(`Server listening on port ${process.env.WEB_SERVER_PORT || 3000}`);
-
-  // ExpressApplication.listen(process.env.WEB_SERVER_PORT || 3000, () => {
-  //   console.info(
-  //     `Server is now running on ${process.env.WEB_SERVER_BASE_URL || "https://localhost"}:${process.env.WEB_SERVER_PORT || 3000}`
-  //   );
-  // });
 }
 
-main();
+// This function starts the application.
+main()
+  .then((r) => r)
+  .catch((error) => console.error(error));
