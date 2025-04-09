5bb8068a84ccb09828593a1419dddb0def3e5704
Matthew <matthew@staff.libraryofcode.org>
2025-04-09 16:00:32 -0400

    beta route for PATCH /transaction/:id

--- backend/routes/transaction.ts
+++ backend/routes/transaction.ts
@@ -94,5 +94,50 @@ export default class TransactionRoute extends Route {
         return this.handleServerError(error as Error, res);
       }
     });
+
+    this.router.patch("/:id", async (req, res) => {
+      try {
+        // check if the required parameters are present
+        if (!req.params.id) return this.sendClientError(res);
+        // check if the account can be found and authenticated
+        const account = await this.authenticate(req, res);
+        if (!account) return this.sendUnauthorized(res);
+        // check if the transaction requested can be located
+        const transaction = await TransactionManager.getTransactionById(req.params.id);
+        if (!transaction) return this.sendNotFound(res);
+        // if type is submitted to update, ensure that the submitted type is typeof TransactionType
+        if (req.body.type && !Object.values(TransactionType).includes(req.body.type)) {
+          return this.sendClientError(res);
+        }
+        // if category is submitted to update, ensure that the submitted category is typeof TransactionCategory
+        if (req.body.category && !Object.values(TransactionCategory).includes(req.body.category)) {
+          return this.sendClientError(res);
+        }
+
+        const updateDetails: {
+          id: string;
+          amount?: number | undefined;
+          category?: TransactionCategory | undefined;
+          descriptor?: string | undefined;
+          type?: TransactionType | undefined;
+        } = {
+          id: transaction.id,
+        };
+
+        if (req.body.amount) updateDetails.amount = req.body.amount;
+        if (req.body.category) updateDetails.category = req.body.category;
+        if (req.body.descriptor) updateDetails.descriptor = req.body.descriptor;
+        if (req.body.type) updateDetails.type = req.body.type;
+
+        const updateQuery = await PrismaDBClient.transaction.update({
+          where: { id: transaction.id },
+          data: updateDetails,
+        });
+        res.status(200).json(updateQuery);
+        return;
+      } catch (error) {
+        return this.handleServerError(error as Error, res);
+      }
+    });
   }
 }
