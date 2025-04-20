ed00aac42c94f7e00bde7e81bd0bafabd1418eb5
Matthew <matthew@staff.libraryofcode.org>
2025-04-20 16:28:22 -0400

    commit migrations

---
+++ backend/prisma/migrations/20250304211337_init/migration.sql
@@ -0,0 +1,81 @@
+-- CreateTable
+CREATE TABLE `Account` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Account_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Budget` (
+    `id` VARCHAR(191) NOT NULL,
+    `category` ENUM('FOOD', 'RENT', 'ENTERTAINMENT', 'UTILITIES', 'TRANSPORTATION', 'HEALTHCARE', 'OTHER', 'INCOME') NOT NULL,
+    `limit` DECIMAL(10, 2) NOT NULL,
+    `accountId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `startDate` DATETIME(3) NOT NULL,
+    `endDate` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Goal` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `targetAmount` DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    `currentSaved` DECIMAL(10, 2) NOT NULL DEFAULT 0,
+    `deadline` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `accountId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `RecurringTransaction` (
+    `id` VARCHAR(191) NOT NULL,
+    `amount` DECIMAL(10, 2) NOT NULL,
+    `category` ENUM('FOOD', 'RENT', 'ENTERTAINMENT', 'UTILITIES', 'TRANSPORTATION', 'HEALTHCARE', 'OTHER', 'INCOME') NOT NULL,
+    `type` ENUM('INCOME', 'EXPENSE') NOT NULL,
+    `descriptor` VARCHAR(191) NOT NULL,
+    `frequency` ENUM('DAILY', 'WEEKLY', 'BIWEEKLY', 'MONTHLY', 'QUARTERLY', 'BIANNUALLY', 'ANNUALLY') NOT NULL,
+    `startDate` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `endDate` DATETIME(3) NOT NULL,
+    `nextDueDate` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `accountId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Transaction` (
+    `id` VARCHAR(191) NOT NULL,
+    `postedAt` DATETIME(3) NOT NULL,
+    `descriptor` VARCHAR(191) NOT NULL,
+    `currency` VARCHAR(191) NOT NULL DEFAULT 'USD',
+    `amount` DECIMAL(10, 2) NOT NULL,
+    `type` ENUM('INCOME', 'EXPENSE') NOT NULL,
+    `category` ENUM('FOOD', 'RENT', 'ENTERTAINMENT', 'UTILITIES', 'TRANSPORTATION', 'HEALTHCARE', 'OTHER', 'INCOME') NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `accountId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Budget` ADD CONSTRAINT `Budget_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `Account`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Goal` ADD CONSTRAINT `Goal_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `RecurringTransaction` ADD CONSTRAINT `RecurringTransaction_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Transaction` ADD CONSTRAINT `Transaction_accountId_fkey` FOREIGN KEY (`accountId`) REFERENCES `Account`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
