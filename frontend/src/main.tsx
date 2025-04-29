e24b239d00e27a25ce4d2d8c46e789ee0d02f43d
Matthew <matthew@staff.libraryofcode.org>
2025-04-28 20:32:29 -0400

    frontend fixes 3x

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from "./App.tsx";
+import App from "./App";
 import './index.css'
-import { NotificationProvider } from './Profile/contexts/NotificationContext.tsx';
+import { NotificationProvider } from './Profile/contexts/NotificationContext';
 
 // Error handling for root rendering
 const rootElement = document.getElementById('root');
